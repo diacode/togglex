0287137d9a014d3d7f70cb8a2cfcec8d788d938d
Victor <hopsor@gmail.com>
2016-03-03 17:33:34 +0100

    Added more dependencies for testing and doc

--- mix.exs
+++ mix.exs
@@ -2,12 +2,20 @@ defmodule Togglex.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :togglex,
-     version: "0.0.1",
-     elixir: "~> 1.2",
-     build_embedded: Mix.env == :prod,
-     start_permanent: Mix.env == :prod,
-     deps: deps]
+    [
+      app: :togglex,
+      version: "0.0.1",
+      elixir: "~> 1.2",
+      build_embedded: Mix.env == :prod,
+      start_permanent: Mix.env == :prod,
+      deps: deps,
+      description: description,
+      package: package,
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: cli_env_for(:test, [
+        "coveralls", "coveralls.detail", "coveralls.post",
+      ])
+    ]
   end
 
   # Configuration for the OTP application
@@ -15,6 +23,12 @@ defmodule Togglex.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:httpoison]]
+  end
+
+  defp description do
+    """
+    Simple Elixir wrapper for the Toggl API
+    """
   end
 
   # Dependencies can be Hex packages:
@@ -29,7 +43,24 @@ defmodule Togglex.Mixfile do
   defp deps do
     [
       {:poison, "~> 2.1.0"},
-      {:httpoison, "~> 0.8.1"}
+      {:httpoison, "~> 0.8.1"},
+      {:ex_doc, "~> 0.11.4"},
+      {:inch_ex, "0.5.1"},
+      {:excoveralls, "~> 0.5.1", only: :test},
+      {:exvcr, "~> 0.7.1", only: :test},
+      {:meck, "~> 0.8.4", only: :test }
     ]
+  end
+
+  defp package do
+    [
+      maintainers: ["hopsor"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/hopsor/togglex"}
+    ]
+  end
+
+  defp cli_env_for(env, tasks) do
+    Enum.reduce(tasks, [], fn(key, acc) -> Keyword.put(acc, :"#{key}", env) end)
   end
 end
