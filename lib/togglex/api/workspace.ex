edfcbe7b16f4b1bf17f9d2af6852145ca69da638
Victor Viruete <hopsor@gmail.com>
2016-03-12 20:30:00 +0100

    Added workspaces api call to retrieve the list of all workspaces where the token used belongs to

--- lib/togglex/api/workspace.ex
+++ lib/togglex/api/workspace.ex
@@ -3,6 +3,20 @@ defmodule Togglex.Api.Workspace do
   alias Togglex.Api.Client
 
   @doc """
+  Returns all the workspaces where the token owner belongs to
+
+  ## Example
+
+    Togglex.Api.Workspace.workspaces(client)
+
+  More info at: https://github.com/toggl/toggl_api_docs/blob/master/chapters/workspaces.md#get-workspaces
+  """
+  @spec workspaces(Client.t) :: Togglex.Response
+  def workspaces(client) do
+    get("workspaces", client, [])
+  end
+
+  @doc """
   Returns all users contained in a specific workspace. To get a successful
   response, the token owner must be workspace admin
 
