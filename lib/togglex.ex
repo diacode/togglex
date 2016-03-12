e66d24b48926fba61e1e8d73f0fe35c34978151c
Victor Viruete <hopsor@gmail.com>
2016-03-12 19:49:05 +0100

    some changes to Togglex module to make possible using Api and Reports clients within it

--- lib/togglex.ex
+++ lib/togglex.ex
@@ -1,6 +1,7 @@
 defmodule Togglex do
   use HTTPoison.Base
-  alias Togglex.Reports.Client
+  alias Togglex.Api.Client, as: ApiClient
+  alias Togglex.Reports.Client, as: ReportsClient
 
   @user_agent [{"User-agent", "togglex"}]
   @type response :: nil | {integer, any} | Poison.Parser.t
@@ -11,27 +12,30 @@ defmodule Togglex do
   def process_response(%HTTPoison.Response{status_code: status_code, body: ""}), do: { status_code, nil }
   def process_response(%HTTPoison.Response{status_code: status_code, body: body }), do: { status_code, Poison.decode!(body, keys: :atoms) }
 
+  @spec get(binary, ApiClient.t | ReportsClient.t, [{atom, binary}] | []) :: response
   def get(path, client, params \\ []) do
     initial_url = url(client, path)
-    url = add_params_to_url(initial_url, Map.put(params, "user_agent", "togglex"))
+    url = add_params_to_url(initial_url, Enum.concat(params, [{"user_agent", "togglex"}]))
     _request(:get, url, client.auth)
   end
 
+  @spec _request(atom, binary, ApiClient.auth | ReportsClient.auth, binary) :: response
   def _request(method, url, auth, body \\ "") do
     request!(method, url, body, authorization_header(auth, @user_agent)) |> process_response
   end
 
-  @spec authorization_header(Client.auth, list) :: list
+  @spec authorization_header(ApiClient.auth | ReportsClient.auth, list) :: list
   def authorization_header(%{access_token: token}, headers) do
     encoded = Base.encode64("#{token}:api_token")
     headers ++ [{"Authorization", "Basic #{encoded}"}]
   end
 
-  @spec url(client :: Client.t, path :: binary) :: binary
-  defp url(_client = %Client{endpoint: endpoint}, path) do
+  @spec url(ApiClient.t | ReportsClient.t, binary) :: binary
+  defp url(%{endpoint: endpoint}, path) do
     endpoint <> "/" <> path
   end
 
+  @spec add_params_to_url(binary, [{atom, binary}]) :: binary
   defp add_params_to_url(url, params) do
     <<url :: binary, build_qs(params) :: binary>>
   end
